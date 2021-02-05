e1023b304532b598f870d00e59ae599be5329acf
Atsushi Togo <atz.togo@gmail.com>
2021-02-05 13:47:46 +0900

    Writing fortran wrapper example for thm-iw

--- fortran/example_f08.f90
+++ fortran/example_f08.f90
@@ -96,6 +96,75 @@ module kspclib_example
   end subroutine get_thm_relative_grid_addresses
 
 
+  subroutine get_thm_integration_weight
+    use kspclib_f08, only: ksp_get_thm_integration_weight, &
+         ksp_get_thm_relative_grid_addresses, ksp_get_all_grid_addresses, &
+         ksp_get_double_grid_address, ksp_get_double_grid_index
+
+    integer :: i, j, k, bi, fi
+    integer(4) :: ga_d(3), ga(3)
+    integer(8) :: gp
+    real(8) :: buf(6000)
+    real(8) :: rec_lattice(3, 3)
+    integer(4) :: relative_grid_addresses(3, 4, 24)
+    integer(4) :: mesh(3), shift(3)
+    integer(4), allocatable :: grid_address(:, :)
+    integer(4) :: tetrahedra_ga(3, 4, 24)
+    integer(8) :: tetrahedra_gps(4, 24)
+    real(8) :: tetrahedra_freqs(4, 24)
+    real(8) :: f
+    real(8) :: dos(8), acc(8)
+
+    dos(:) = 0
+    acc(:) = 0
+
+    mesh(:) = 10
+    shift(:) = 0
+    allocate(grid_address(3, product(mesh)))
+    call ksp_get_all_grid_addresses(grid_address, mesh)
+
+    rec_lattice(:, :) = 0.1757376132331179
+    rec_lattice(1, 1) = -0.1757376132331179
+    rec_lattice(2, 2) = -0.1757376132331179
+    rec_lattice(3, 3) = -0.1757376132331179
+    call ksp_get_thm_relative_grid_addresses(relative_grid_addresses, &
+         rec_lattice)
+
+    open(17, file='../python/test/NaCl-freqs-101010.dat', status='old')
+    do i = 1, 6000
+       read (17, *) buf(i)
+    end do
+    close(17)
+
+    ! do i = 1, 1000
+    !    print '(6f12.8)', buf((i - 1)*6 + 1:  i * 6)
+    ! end do
+
+    do i = 1, product(mesh)
+       do fi = 1, 8
+          do bi = 1, 6
+             do j = 1, 24
+                do k = 1, 4
+                   ga = relative_grid_addresses(:, k, j) + grid_address(:, i)
+                   call ksp_get_double_grid_address(ga_d, ga, mesh, shift)
+                   gp = ksp_get_double_grid_index(ga_d, mesh)
+                   tetrahedra_freqs(k, j) = buf(gp * 6 + bi)
+                end do
+             end do
+             f = fi - 1
+             dos(fi) = dos(fi) + &
+                  ksp_get_thm_integration_weight(f, tetrahedra_freqs, 'J')
+          end do
+       end do
+    end do
+
+    do fi = 1, 8
+       print *, dos(fi) / 1000
+    end do
+
+  end subroutine get_thm_integration_weight
+
+
   subroutine snf3x3(A)
     use kspclib_f08, only: ksp_get_snf3x3
 
@@ -118,6 +187,7 @@ module kspclib_example
     print *, "Q", Q(3, :)
     print *, ""
   end subroutine snf3x3
+
 end module kspclib_example
 
 
@@ -125,7 +195,7 @@ program kspclib_example_f08
 
   use kspclib_example, only: snf3x3, kspclib_version, get_all_grid_addresses, &
        get_double_grid_address, get_double_grid_index, &
-       get_thm_relative_grid_addresses
+       get_thm_relative_grid_addresses, get_thm_integration_weight
 
   integer(8) :: A(3, 3)
   integer(4) :: mesh(3), address(3), is_shift(3), address_double(3)
@@ -158,5 +228,6 @@ program kspclib_example_f08
   call get_double_grid_address(address, mesh, is_shift)
   call get_double_grid_index(address_double, mesh)
   call get_thm_relative_grid_addresses(rec_lattice)
+  call get_thm_integration_weight()
 
 end program kspclib_example_f08
--- fortran/kspclib_f08.f90
+++ fortran/kspclib_f08.f90
@@ -394,6 +394,16 @@ module kspclib_f08
      end subroutine ksp_get_thm_relative_grid_addresses
 
 
+     function ksp_get_thm_integration_weight(omega, &
+          tetrahedra_omegas, function_char) bind(c)
+       import c_char, c_double
+       real(c_double), intent(in) :: omega
+       real(c_double), intent(in) :: tetrahedra_omegas(4, 24)
+       character(kind=c_char), intent(in) :: function_char
+       real(c_double) :: ksp_get_thm_integration_weight
+     end function ksp_get_thm_integration_weight
+
+
      function ksp_get_snf3x3(D_diag, P, Q, A) bind(c)
        import c_long, c_int
        integer(c_long), intent(inout) :: D_diag(3)
@@ -408,6 +418,7 @@ module kspclib_f08
   public :: ksp_get_major_version,  ksp_get_minor_version, &
        ksp_get_micro_version, ksp_get_all_grid_addresses, ksp_get_snf3x3, &
        ksp_get_double_grid_address, ksp_get_double_grid_index, &
-       ksp_get_thm_relative_grid_addresses
+       ksp_get_thm_relative_grid_addresses, ksp_get_thm_integration_weight
+
 
 end module kspclib_f08
